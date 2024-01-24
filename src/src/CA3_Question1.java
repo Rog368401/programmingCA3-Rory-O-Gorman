c03167504a20806c22c167850b3d8c3bce90fdf8
Rog368401 <roryogorman36@gmail.com>
2024-01-24 09:47:16 +0000

    added sample code
    took the sample code provided and added it to my main

---
+++ src/src/CA3_Question1.java
@@ -0,0 +1,27 @@
+import java.util.ArrayDeque;
+import java.util.Deque;
+import java.util.Scanner;
+
+/**
+ *  Name: Rory O'Gorman
+ *  Class Group: SD2B
+ */
+public class CA3_Question1
+{
+    public static void runSimulation()
+    {
+driveway();
+street();
+    }
+public void driveway()
+{
+    Deque<String> myStack = new ArrayDeque<>();
+}
+public void street()
+{
+
+}
+    public static void main(String[] args) {
+        runSimulation();
+    }
+}
