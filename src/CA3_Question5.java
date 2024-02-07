7ecfbf7bf52902ed0f7ec15b5f0c38afb2938900
Rog368401 <roryogorman36@gmail.com>
2024-02-07 09:46:02 +0000

    question 5 set up to start switch statement

--- src/CA3_Question5.java
+++ src/CA3_Question5.java
@@ -1,4 +1,8 @@
 package src;
+
+import java.util.LinkedList;
+import java.util.Queue;
+
 /**
  *  Name:
  *  Class Group:
@@ -6,6 +10,17 @@ package src;
 
 public class CA3_Question5
 {
+    public static void airport(){
+        Queue<String> takeOff = new LinkedList<String>();
+        Queue<String> land = new LinkedList<String>();
+        System.out.println("What would you like to do?");
+        System.out.println(" 1- Enter flight for takeOff");
+        System.out.println(" 2- Enter flight for landing");
+        System.out.println("3- show takeOff Queue");
+        System.out.println("4- show Landing Queue");
+        System.out.println("5- Quit");
+    }
+
 
     public static void main(String[] args)
     {
