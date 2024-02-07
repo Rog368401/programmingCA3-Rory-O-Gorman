c047d41b6782ef5134bcffc5f0fc70a24e87c655
Rog368401 <roryogorman36@gmail.com>
2024-02-07 10:06:05 +0000

    switch statement for question5

--- src/CA3_Question5.java
+++ src/CA3_Question5.java
@@ -2,6 +2,7 @@ package src;
 
 import java.util.LinkedList;
 import java.util.Queue;
+import java.util.Scanner;
 
 /**
  *  Name:
@@ -11,19 +12,45 @@ import java.util.Queue;
 public class CA3_Question5
 {
     public static void airport(){
+        Scanner kbrd = new Scanner(System.in);
         Queue<String> takeOff = new LinkedList<String>();
         Queue<String> land = new LinkedList<String>();
-        System.out.println("What would you like to do?");
-        System.out.println(" 1- Enter flight for takeOff");
-        System.out.println(" 2- Enter flight for landing");
-        System.out.println("3- show takeOff Queue");
-        System.out.println("4- show Landing Queue");
-        System.out.println("5- Quit");
+        int choice;
+        do {
+
+
+            System.out.println("What would you like to do?");
+            System.out.println(" 1- Enter flight for takeOff");
+            System.out.println(" 2- Enter flight for landing");
+            System.out.println("0- Quit");
+
+            choice = kbrd.nextInt();
+
+            switch (choice) {
+                case 0:{
+                    System.out.println("Take off queue = " +
+                            takeOff );
+                    System.out.println("Landing queue = " + land);
+                }
+                break;
+                case 1: {
+                    System.out.println("Please enter flight number");
+                    takeOff.add(kbrd.next());
+                    System.out.println("flights ready for takeOff " + takeOff);
+                }
+                break;
+                case 2: {
+                    System.out.println("Please enter flight number");
+                    land.add(kbrd.next());
+                    System.out.println("flights to land " + land);
+                }
+            }
+        } while (choice > 0);
     }
 
 
     public static void main(String[] args)
     {
-
+        airport();
     }
 }
