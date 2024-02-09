34b7a7e69e10d0c5b2cdc3ba36c12b72e558bb5a
Rog368401 <roryogorman36@gmail.com>
2024-02-09 10:44:16 +0000

    changes made to Q6

--- src/CA3_Question6.java
+++ src/CA3_Question6.java
@@ -20,19 +20,23 @@ public class CA3_Question6
     public static void main(String[] args) {
         Queue<Integer> shares = new LinkedList<>();
         Queue<Integer> gain = new LinkedList<>();
-        int total;
+        double total = 0;
        Scanner in = new Scanner(System.in);
         String command="";
             do {
+                System.out.println("Would you like to Buy, Sell or Quit");
             System.out.print(">");
             command = in.next();
             if(command.equalsIgnoreCase("buy"))
             {
+                System.out.println("How many shares would you like to buy");
                 int qty = in.nextInt();
+                System.out.println("How much do they cost per share");
                 double price = in.nextDouble();
-                total = (int) (price*qty);
-                shares.add(total);
-                System.out.println( "current shares" + shares);
+                total = total+(price * qty);
+                shares.add(qty);
+
+                System.out.println( "current shares " + shares + "Total spent " + total);
             }
             else if(command.equals("sell"))
             {
